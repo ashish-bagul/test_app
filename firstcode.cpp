29649bb4c30ce4dbf61350a3bfb489012f2be265
ashish-bagul <ashishb@heaptrace.com>
2025-03-26 18:14:52 +0530

    First code to print 3 lines of message.

---
+++ firstcode.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string name;
+    cout << "Enter your name: ";
+    cin >> name;
+    cout << "Hello " << name << "!" << endl;
+    cout << "Good evening!" << endl;
+    cout << "Happy Programming!" << endl;
+    return 0; 
+}
